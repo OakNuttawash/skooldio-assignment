78550243f06f815a61d86d46eefbb6226ee9c921
OakNuttawash <oak.mirthful@gmail.com>
2024-12-28 14:57:07 +0700

    feat: integate with api

--- src/common/api/client.ts
+++ src/common/api/client.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://tcas-assets.skooldio.com/tmp/",
+  baseURL: "https://tcas-assets.skooldio.com/tmp/mock_tcaster_api.json",
   headers: {
     "Content-Type": "application/json",
   },
--- src/components/card.tsx
+++ src/components/card.tsx
@@ -1,15 +1,13 @@
-import { ApiResponse } from "@/common/api/types";
-import { Button } from "@/components/ui/button";
 import { Separator } from "@/components/ui/separator";
 import Typography from "@/components/ui/typography";
 import FavoriteIcon from "@mui/icons-material/Favorite";
 import PersonIcon from "@mui/icons-material/Person";
 import PlayArrowIcon from "@mui/icons-material/PlayArrow";
 
-interface CardProps {
-  item: ApiResponse;
-}
-export default function Card({ items }: CardProps) {
+// interface CardProps {
+//   item?: ApiResponse;
+// }
+export default function Card() {
   return (
     <div className="flex flex-col shadow rounded-md px-4 py-5 w-full max-w-[442px] gap-5">
       <div className="flex gap-2.5 w-full">
@@ -19,22 +17,26 @@ export default function Card({ items }: CardProps) {
           className="object-contain w-20"
         />
         <div className="flex flex-col gap-6 w-full">
-          <div className="flex flex-col gap-2 w-full">
+          <div className="flex flex-col w-full">
             <div className="flex justify-between w-full items-center">
-              <Typography variant="h1" className="text-grapefruit">
+              <Typography className="text-grapefruit text-2xl">
                 คณะวิศวกรรมศาสตร์
               </Typography>
               <FavoriteIcon className="w-1.5 h-1.5 text-grapefruit" />
             </div>
             <Typography
               variant="body1"
-              fontWeight="light"
-              className="text-warm-grey"
+              fontWeight="medium"
+              className="text-warm-grey text-xl"
             >
               สาขาวิศวกรรมทั่วไป
             </Typography>
           </div>
-          <Typography variant="body1" className="text-warm-grey">
+          <Typography
+            variant="body1"
+            fontWeight="light"
+            className="text-warm-grey text-xl"
+          >
             จุฬาลงกรณ์มหาวิทยาลัย
           </Typography>
         </div>
@@ -42,7 +44,11 @@ export default function Card({ items }: CardProps) {
       <Separator />
       <div className="flex flex-col gap-3">
         <div className="flex gap-6 items-center">
-          <Typography variant="body1" className="text-grey-two">
+          <Typography
+            variant="body1"
+            fontWeight="light"
+            className="text-grey-two text-xl"
+          >
             รอบที่เปิด
           </Typography>
           <div className="flex gap-2 items-center">
@@ -59,64 +65,74 @@ export default function Card({ items }: CardProps) {
           </div>
         </div>
         <div className="flex justify-between items-center">
-          <Typography variant="body1" className="text-grapefruit">
+          <Typography
+            variant="body1"
+            className="text-grapefruit text-base font-semibold"
+          >
             รอบที่ 4 : Admission
           </Typography>
-          <Button>แก้ไขคะแนน</Button>
+          <button className="px-5 py-2 text-xs border flex gap-2 rounded-2xl bg-transparent border-grapefruit text-grapefruit">
+            แก้ไขคะแนน
+            <img src="/src/assets/cal.svg" alt="cal" className="h-4" />
+          </button>
         </div>
         <div className="flex justify-between items-center pt-2">
           <div className="flex justify-center">
-            <img src="/src/assets/medal.svg" alt="medal" className="w-7 ml-6" />
+            <img
+              src="/src/assets/medal.svg"
+              alt="medal"
+              className="w-7 h-9 ml-6"
+            />
           </div>
           <div className="flex flex-col items-end w-1/3">
             <Typography
               variant="body1"
               fontWeight="light"
-              className="text-grey-three"
+              className="text-grey-three text-xs"
             >
               คะแนนของคุณคือ
             </Typography>
             <Typography
               variant="h1"
               fontWeight="light"
-              className="text-grey-three"
+              className="text-grey-three text-4xl"
             >
               22222
             </Typography>
           </div>
         </div>
-        <div className="flex h-5 items-center space-x-4 text-sm">
-          <div className="flex flex-col">
-            <Typography variant="body1" className="text-grey-two">
+        <div className="flex h-5 items-center space-x-4 justify-center my-4 w-full">
+          <div className="flex flex-col items-center w-full">
+            <Typography variant="body1" className="text-grey-two text-xl">
               20,845
             </Typography>
-            <Typography variant="body1" className="text-grey-two">
+            <Typography variant="body1" className="text-grey-two text-xs">
               คะแนนต่ำสุด 60
             </Typography>
           </div>
           <Separator orientation="vertical" />
-          <div className="flex flex-col">
-            <Typography variant="body1" className="text-grey-two">
+          <div className="flex flex-col items-center w-full">
+            <Typography variant="body1" className="text-grey-two text-xl">
               20,845
             </Typography>
-            <Typography variant="body1" className="text-grey-two">
-              คะแนนต่ำสุด 60
+            <Typography variant="body1" className="text-grey-two text-xs">
+              คะแนนเฉลี่ย 60
             </Typography>
           </div>
           <Separator orientation="vertical" />
-          <div className="flex flex-col">
-            <Typography variant="body1" className="text-grey-two">
+          <div className="flex flex-col items-center w-full">
+            <Typography variant="body1" className="text-grey-two text-xl">
               20,845
             </Typography>
-            <Typography variant="body1" className="text-grey-two">
-              คะแนนต่ำสุด 60
+            <Typography variant="body1" className="text-grey-two text-xs">
+              คะแนนสูงสุด 60
             </Typography>
           </div>
         </div>
         <Separator />
         <div className="flex gap-5 items-center">
           <PlayArrowIcon className="text-[#48b6a3]" />
-          <Typography variant="body1" className="text-[#48b6a3]">
+          <Typography variant="body1" className="text-[#48b6a3] text-base">
             ดูสัดส่วนคะแนน
           </Typography>
         </div>
@@ -124,7 +140,7 @@ export default function Card({ items }: CardProps) {
         <div className="flex justify-between items-center">
           <div className="flex gap-2 items-center">
             <PersonIcon className="text-warm-grey" />
-            <Typography variant="body1" className="text-warm-grey">
+            <Typography variant="body1" className="text-warm-grey text-xs">
               10 คนที่สนใจ
             </Typography>
           </div>
